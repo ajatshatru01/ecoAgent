1d014d0e12aa64c5c1cff5750c5c9bf16b0fa515
UTSAV OJHA <ajatsatru787@gmail.com>
2025-11-29 01:03:52 +0530

    Added chat in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CollectInfo from './pages/CollectInfo';
-
+import Chat from './pages/Chat';
 function App() {
   return (
     <Routes>
       <Route path = "/" element={<Home />}/>
       <Route path = "/info" element={<CollectInfo />}/>
+      <Route path = "/chat" element={<Chat />}/>
     </Routes>
   )
 }
