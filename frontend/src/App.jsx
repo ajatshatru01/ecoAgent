6beaf7ee54929b2151a4cd45445b0b929647b223
OP-T4RUN <tarunbajotra2006@gmail.com>
2025-11-28 23:09:47 +0530

    Added collect info

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+//simport CollectInfoPage from './pages/CollectInfoPage';
+import CollectInfoWizard from './pages/CollectInfo';
 
 function App() {
   return (
     <Routes>
       <Route path = "/" element={<Home />}/>
+      <Route path = "/info" element={<CollectInfoWizard />}/>
     </Routes>
   )
 }
