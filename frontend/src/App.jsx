2749df46f950dac4239c04740f77979b602f9c67
UTSAV OJHA <ajatsatru787@gmail.com>
2025-11-29 01:32:26 +0530

    Added resultpage route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CollectInfo from './pages/CollectInfo';
 import Chat from './pages/Chat';
+import ResultPage from './pages/ResultPage';
 function App() {
   return (
     <Routes>
       <Route path = "/" element={<Home />}/>
       <Route path = "/info" element={<CollectInfo />}/>
       <Route path = "/chat" element={<Chat />}/>
+      <Route path = "/result" element={<ResultPage />}/>
     </Routes>
   )
 }
